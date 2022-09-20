9e6f7d233c7c1a4ea953555491322a5677570335
LHDylan <lhdylan@hotmail.com>
2022-09-20 11:52:28 +0200

    feat: finished editing the CRUD, no issues, verified in PostMan

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/api/personnages', name: 'api_')]
 class PersonnageController extends AbstractController
@@ -20,20 +21,32 @@ class PersonnageController extends AbstractController
         $personnages = $personnageRepository->findAll();
 
         return $this->json([
-            $personnages
+            'personnages' => $personnages
         ]);
     }
 
-    #[Route('/:id', name: 'app_personnage_show', methods: ['GET'])]
-    public function show(Personnage $personnage): JsonResponse
+    #[Route('/{id}', name: 'app_personnage_show', methods: ['GET'])]
+    public function show(Personnage $personnage, PersonnageRepository $personnageRepository, int $id): JsonResponse
     {
-        return $this->json([
-            $personnage
-        ]);
+        $personnage = $personnageRepository->findOneBy(['id' => $id]);
+
+        $data = [
+            'id' => $personnage->getId(),
+            'name' => $personnage->getName(),
+            'stand' => $personnage->getStand(),
+            'birthday' => $personnage->getBirthday(),
+            'gender' => $personnage->getGender(),
+            'height' => $personnage->getHeight(),
+            'weight' => $personnage->getWeight(),
+            'anime' => $personnage->getAnime(),
+            'description' => $personnage->getDescription(),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
     }
 
-    #[Route('/new', name: 'app_personnage_new', methods: ['POST'])]
-    public function new(Request $request, SerializerInterface $serializer,  PersonnageRepository $personnageRepository): JsonResponse
+    #[Route('/', name: 'app_personnage_new', methods: ['POST'])]
+    public function new(Request $request, SerializerInterface $serializer, PersonnageRepository $personnageRepository): JsonResponse
     {
 
         $personnage = $serializer->deserialize($request->getContent(), Personnage::class, 'json');
@@ -48,5 +61,41 @@ class PersonnageController extends AbstractController
         return $this->json([
             'message' => 'New character was created successfully.'
         ], 201);
+    }
+
+    #[Route('/{id}', name: 'app_personnage_edit', methods: ['PUT'])]
+    public function edit(Request $request, PersonnageRepository $personnageRepository, int $id): JsonResponse
+    {
+        $personnage = $personnageRepository->findOneBy(['id' => $id]);
+        $data = json_decode($request->getContent(), true);
+
+        empty($data['name']) ? true : $personnage->setName($data['name']);
+        empty($data['stand']) ? true : $personnage->setStand($data['stand']);
+        empty($data['birthday']) ? true : $personnage->setBirthday(new \DateTime($data['birthday']));
+        empty($data['gender']) ? true : $personnage->setGender($data['gender']);
+        empty($data['height']) ? true : $personnage->setHeight($data['height']);
+        empty($data['weight']) ? true : $personnage->setWeight($data['weight']);
+        empty($data['anime']) ? true : $personnage->setAnime($data['anime']);
+        empty($data['description']) ? true : $personnage->setDescription($data['description']);
+
+        $personnageRepository->add($personnage, true);
+
+        // return new JsonResponse($personnage, Response::HTTP_OK);
+
+        return $this->json([
+            'personnage' => $personnage
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_personnage_delete', methods: ['DELETE'])]
+    public function delete(PersonnageRepository $personnageRepository, int $id): JsonResponse
+    {
+        $personnage = $personnageRepository->findOneBy(['id' => $id]);
+
+        $personnageRepository->remove($personnage, true);
+
+        return $this->json([
+            'message' => 'Personnage deleted'
+        ]);
     }
 }
--- src/Entity/Personnage.php
+++ src/Entity/Personnage.php
@@ -6,7 +6,7 @@ use App\Repository\PersonnageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ORM\Entity(repositoryClass: PersonnageRepository::class)]
 class Personnage
 {
     #[ORM\Id]
